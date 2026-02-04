cc596d27cf1dcb409019a86ed812f62d7ea06183
aahilrupsi <aahil2farid@gmail.com>
2026-02-04 11:38:08 -0500

    set up and commented config files - ready for actua; proj now, also added wallpaper

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// takes our app and puts in <div id="root"></div> in index.html
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +15,16 @@ export default defineConfig({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
-    }),
+    }), tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '#components': resolve(__dirname, 'components'),
+      '#constants': resolve(__dirname, 'constants'),
+      '#store': resolve(__dirname, 'store'),
+      '#hoc': resolve(__dirname, 'hoc'),
+      '#windows': resolve(__dirname, 'windows'),
+      // this helps us import files from any location in the project without using relative paths
+    },
+  },
 })
